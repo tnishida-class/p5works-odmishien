1076a1fc6cd3cef25bbc6ad3d77850719ff6b7ed
odmishien <tmtm.yagi.007@gmail.com>
2020-10-26 18:43:57 +0900

    2-q3

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -17,19 +17,23 @@ function setup() {
 
   drawCircle(black, maxR);
   drawArcs(green, red, maxR * 0.8);
-  // BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
+  drawArcs(cream, black, maxR * 0.75);
+  drawArcs(green, red, maxR * 0.5);
+  drawArcs(cream, black, maxR * 0.45);
+
+  drawCircle(green, maxR * 0.1);
   drawCircle(red, maxR * 0.05);
 }
 
-function drawCircle(c, r){
+function drawCircle(c, r) {
   fill(c);
   ellipse(cx, cy, r, r);
 }
 
 function drawArcs(c1, c2, r) {
   for (let i = 0; i < 20; i++) {
-    let start = TWO_PI / 20 * i;
-    let stop = TWO_PI / 20 * (i + 1);
+    let start = (TWO_PI / 20) * i;
+    let stop = (TWO_PI / 20) * (i + 1);
     fill(i % 2 == 0 ? c1 : c2);
     arc(cx, cy, r, r, start, stop, PIE);
   }
